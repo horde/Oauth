37f6e045aa22585a1e3415ddfb1ead087e3804b6
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-18 13:43:12 +0100

    feat: Implement TokenIssuers for access token and refresh token

---
+++ src/Server/Token/RefreshTokenIssuer.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2026 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
+ *
+ * @author   Ralf Lang <ralf.lang@ralf-lang.de>
+ */
+
+namespace Horde\Oauth\Server\Token;
+
+use Horde\Oauth\Server\Entity\Client;
+use Horde\Oauth\Server\Entity\RefreshToken;
+use Horde\Oauth\Server\Entity\Scope;
+use Horde\Oauth\Server\Repository\RefreshTokenRepository;
+
+final class RefreshTokenIssuer
+{
+    public function __construct(
+        private readonly RefreshTokenRepository $repository,
+        private readonly int $ttl = 2592000,
+    ) {}
+
+    /**
+     * @param Scope[] $scopes
+     */
+    public function issue(string $accessTokenId, Client $client, ?string $identityId, array $scopes): string
+    {
+        $tokenId = bin2hex(random_bytes(32));
+        $expiresAt = new \DateTimeImmutable("+{$this->ttl} seconds");
+
+        $entity = new RefreshToken(
+            $tokenId,
+            $accessTokenId,
+            $client->clientId,
+            $identityId,
+            Scope::toSpaceSeparated($scopes),
+            $expiresAt,
+        );
+        $this->repository->persist($entity);
+
+        return $tokenId;
+    }
+}
