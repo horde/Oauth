5ac69553fc16a2d941e3d6505926a63f3514e91c
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-18 16:55:54 +0100

    feat: Exception handling

---
+++ src/Exception/InvalidGrantException.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2026 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
+ *
+ * @author   Ralf Lang <ralf.lang@ralf-lang.de>
+ */
+
+namespace Horde\Oauth\Exception;
+
+final class InvalidGrantException extends OAuthException
+{
+    public function __construct(
+        string $errorDescription = '',
+        string $errorUri = '',
+        ?\Throwable $previous = null,
+    ) {
+        parent::__construct('invalid_grant', $errorDescription, $errorUri, 400, $previous);
+    }
+}
