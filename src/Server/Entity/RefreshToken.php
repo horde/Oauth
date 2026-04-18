06be7d3746d015cf870053dd87d11fb0ddebdaf4
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-18 14:39:16 +0100

    feat: Draft consent, access token, refresh token, scope and authorization code entities

---
+++ src/Server/Entity/RefreshToken.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2026 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
+ *
+ * @author   Ralf Lang <ralf.lang@ralf-lang.de>
+ */
+
+namespace Horde\Oauth\Server\Entity;
+
+final class RefreshToken
+{
+    public function __construct(
+        public readonly string $tokenId,
+        public readonly string $accessTokenId,
+        public readonly string $clientId,
+        public readonly ?string $identityId,
+        public readonly string $scope,
+        public readonly \DateTimeImmutable $expiresAt,
+        public readonly bool $revoked = false,
+    ) {}
+
+    public function isExpired(): bool
+    {
+        return new \DateTimeImmutable() >= $this->expiresAt;
+    }
+
+    public function isRevoked(): bool
+    {
+        return $this->revoked;
+    }
+}
