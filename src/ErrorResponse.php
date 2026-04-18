8b8705ef9c2eb685a7cdd04e16a8a25fa0174ff9
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-18 16:55:54 +0100

    feat: Exception handling

---
+++ src/ErrorResponse.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2026 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
+ *
+ * @author   Ralf Lang <ralf.lang@ralf-lang.de>
+ */
+
+namespace Horde\Oauth;
+
+use Horde\Oauth\Exception\OAuthException;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+
+final class ErrorResponse
+{
+    /**
+     * @return array<string, string>
+     */
+    public static function toArray(OAuthException $e): array
+    {
+        $data = ['error' => $e->getError()];
+
+        if ($e->getErrorDescription() !== '') {
+            $data['error_description'] = $e->getErrorDescription();
+        }
+
+        if ($e->getErrorUri() !== '') {
+            $data['error_uri'] = $e->getErrorUri();
+        }
+
+        return $data;
+    }
+
+    public static function toJson(OAuthException $e): string
+    {
+        return json_encode(self::toArray($e), JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES);
+    }
+
+    public static function toResponse(
+        OAuthException $e,
+        ResponseFactoryInterface $responseFactory,
+        StreamFactoryInterface $streamFactory,
+    ): ResponseInterface {
+        $body = $streamFactory->createStream(self::toJson($e));
+
+        return $responseFactory->createResponse($e->getHttpStatusCode())
+            ->withHeader('Content-Type', 'application/json;charset=UTF-8')
+            ->withHeader('Cache-Control', 'no-store')
+            ->withHeader('Pragma', 'no-cache')
+            ->withBody($body);
+    }
+}
---
+++ src/Exception/OAuthException.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2026 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
+ *
+ * @author   Ralf Lang <ralf.lang@ralf-lang.de>
+ */
+
+namespace Horde\Oauth\Exception;
+
+class OAuthException extends \RuntimeException
+{
+    public function __construct(
+        private readonly string $error,
+        string $errorDescription = '',
+        private readonly string $errorUri = '',
+        private readonly int $httpStatusCode = 400,
+        ?\Throwable $previous = null,
+    ) {
+        parent::__construct($errorDescription, 0, $previous);
+    }
+
+    public function getError(): string
+    {
+        return $this->error;
+    }
+
+    public function getErrorDescription(): string
+    {
+        return $this->getMessage();
+    }
+
+    public function getErrorUri(): string
+    {
+        return $this->errorUri;
+    }
+
+    public function getHttpStatusCode(): int
+    {
+        return $this->httpStatusCode;
+    }
+}
