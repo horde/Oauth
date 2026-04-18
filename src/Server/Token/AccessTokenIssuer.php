d54e64bec97be384b335a7b3856e11555d21fbb4
Ralf Lang <ralf.lang@ralf-lang.de>
2026-04-18 13:43:12 +0100

    feat: Implement TokenIssuers for access token and refresh token

---
+++ src/Server/Token/AccessTokenIssuer.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2026 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (BSD). If you
+ * did not receive this file, see http://www.horde.org/licenses/bsd.
+ *
+ * @author   Ralf Lang <ralf.lang@ralf-lang.de>
+ */
+
+namespace Horde\Oauth\Server\Token;
+
+use Horde\Jwt\Signer\SignerInterface;
+use Horde\Jwt\TokenEncoder;
+use Horde\Oauth\Server\Entity\AccessToken;
+use Horde\Oauth\Server\Entity\Client;
+use Horde\Oauth\Server\Entity\Scope;
+use Horde\Oauth\Server\Repository\AccessTokenRepository;
+
+final class AccessTokenIssuer
+{
+    public function __construct(
+        private readonly TokenEncoder $encoder,
+        private readonly SignerInterface $signer,
+        private readonly AccessTokenRepository $repository,
+        private readonly string $issuer,
+        private readonly int $ttl = 3600,
+    ) {}
+
+    /**
+     * @param Scope[] $scopes
+     * @return array<string, mixed>
+     */
+    public function issue(Client $client, ?string $identityId, array $scopes): array
+    {
+        $jti = bin2hex(random_bytes(16));
+        $scopeString = Scope::toSpaceSeparated($scopes);
+        $expiresAt = new \DateTimeImmutable("+{$this->ttl} seconds");
+
+        $claims = [
+            'iss' => $this->issuer,
+            'sub' => $identityId,
+            'aud' => $client->clientId,
+            'jti' => $jti,
+            'scope' => $scopeString,
+            'client_id' => $client->clientId,
+        ];
+
+        $token = $this->encoder->encode($claims, $this->signer, $this->ttl);
+
+        $entity = new AccessToken(
+            $jti,
+            $client->clientId,
+            $identityId,
+            $scopeString,
+            $expiresAt,
+        );
+        $this->repository->persist($entity);
+
+        $result = [
+            'access_token' => $token->toString(),
+            'token_type' => 'Bearer',
+            'expires_in' => $this->ttl,
+        ];
+
+        if ($scopeString !== '') {
+            $result['scope'] = $scopeString;
+        }
+
+        $result['_jti'] = $jti;
+
+        return $result;
+    }
+}
